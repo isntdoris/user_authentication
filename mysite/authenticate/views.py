b0d931e1a2ffb1ea93eb8eb2133657db3051039e
isntdoris <isntdoris@gmail.com>
2024-05-10 21:17:26 -0500

    edit profile

--- mysite/authenticate/views.py
+++ mysite/authenticate/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib import messages
 from .forms import SingUpForm
 
@@ -42,3 +42,6 @@ def register_user(request):
         form = SingUpForm()
     context = {'form': form}
     return render(request, 'authenticate/register.html', context)
+
+def edit_profile(request):
+    return render(request, 'authenticate/edit_profile.html', {})
