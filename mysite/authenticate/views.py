181acfb0dfc121c9c1c7366bed78f55e8f304342
isntdoris <isntdoris@gmail.com>
2024-05-10 20:38:24 -0500

    user registration

--- mysite/authenticate/views.py
+++ mysite/authenticate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SingUpForm
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
@@ -24,3 +26,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You Have Been Logged Out...'))
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SingUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.changed_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You Have Registered...'))
+            return redirect('home')
+    else:
+        form = SingUpForm()
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
